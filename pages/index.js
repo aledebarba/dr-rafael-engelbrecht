45b51911aae447fea64d4790a59cdd9347b56e9b
Alexandre Macedo <ale@alemacedo.com>
2021-01-11 11:56:48 -0300

    links update and minor design changes

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,57 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '../components/navbar'
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/navbar";
 
 export default function Home() {
-  
   return (
     <div className="content">
       <Head>
         <title>Dr Rafael Hengelbrecht</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar menu={[
-          {text:"Home", url:"#top"},
-          {text:"Áreas de atuação", url:"#areas"},
-          {text:"Formação", url:"#formacao"},
-          {text:"Publicação", url:"#publicacao"},
-          {text:"Atendimento", url:"#atendimento"},
-          {text:"Contato", url:"#call_to_action"}
-      ]}/>
-        <div className="container-full hero">
-          <div className="container">
-            <div className="row">
-              <div className="col w50">
-                <h1>Dr Rafael Engelbrecht</h1>
-                <h2>Cirurgião Vascular e Endovascular</h2>
-                <h3>CRM 156519 | RQE 88339 | RQE 883391</h3>
-                <p>Rafael Engelbrecht é formado em Medicina pela Escola Paulista de Medicina - Universidade Federal de São Paulo e possui residência médica nas áreas de Cirurgia Geral, Cirurgia Vascular e Cirurgia Endovascular pela mesma Universidade.</p>
-                <a href="#areas" className="button primary">
-                  Saiba mais
-                </a>
-              </div>
-              <div className="col w50">
-                <img src="/images/hero__main-image.png" alt="foto do dr rafael engelbrecht"/>
-              </div>
+      <Navbar
+        menu={[
+          { text: "Home", url: "#top" },
+          { text: "Áreas de atuação", url: "#areas" },
+          { text: "Formação", url: "#formacao" },
+          { text: "Publicação", url: "#publicacao" },
+          { text: "Atendimento", url: "#atendimento" },
+          { text: "Contato", url: "#call_to_action" },
+        ]}
+      />
+      <div className="container-full hero">
+        <div className="container">
+          <div className="row">
+            <div className="col w50">
+              <h1>Dr Rafael Engelbrecht</h1>
+              <h2>Cirurgião Vascular e Endovascular</h2>
+              <h3>CRM 156519 | RQE 88339 | RQE 883391</h3>
+              <p>
+                Rafael Engelbrecht é formado em Medicina pela Escola Paulista de
+                Medicina - Universidade Federal de São Paulo e possui residência
+                médica nas áreas de Cirurgia Geral, Cirurgia Vascular e Cirurgia
+                Endovascular pela mesma Universidade.
+              </p>
+              <a href="#areas" className="button primary">
+                Saiba mais
+              </a>
+            </div>
+            <div className="col w50">
+              <img
+                src="/images/hero__main-image.png"
+                alt="foto do dr rafael engelbrecht"
+              />
             </div>
           </div>
         </div>
+      </div>
 
       <div id="areas">
         <div className="container">
           <h1>Áreas de Atuação</h1>
-          <h2>PRINCIPAIS ATUAÇÕES, TÉCNICAS E ÁREAS DE ATENDIMENTO ESPECIALIZADO</h2>
+          <h2>
+            PRINCIPAIS ATUAÇÕES, TÉCNICAS E ÁREAS DE ATENDIMENTO ESPECIALIZADO
+          </h2>
           <div className="row">
             <div className="col areas__text">
               <div className="area">
@@ -53,7 +64,9 @@ export default function Home() {
                 <h3>Doenças Venosas</h3>
                 <ul>
                   <li>Insuficiência venosa</li>
-                  <li>Varizes, vasinhos - tratamento cirúrgico e escleroterapia</li>
+                  <li>
+                    Varizes, vasinhos - tratamento cirúrgico e escleroterapia
+                  </li>
                   <li>Trombose venosa profunda (TVP), tromboflebite</li>
                 </ul>
               </div>
@@ -76,139 +89,233 @@ export default function Home() {
                 <ul>
                   <li>Cateteres: Port-o-cath, Permcath, PICC</li>
                   <li>Fístula arteriovenosa para hemodiálise</li>
-                  <li>Doenças linfáticas (congênitas, adquiridas e pós-infecciosas)</li>
-                  <li>Doenças/Síndromes reumatológicas e auto-imunes com acometimento vascular</li>
+                  <li>
+                    Doenças linfáticas (congênitas, adquiridas e
+                    pós-infecciosas)
+                  </li>
+                  <li>
+                    Doenças/Síndromes reumatológicas e auto-imunes com
+                    acometimento vascular
+                  </li>
                   <li>Check-up vascula</li>
                 </ul>
               </div>
-              <a href="https://linkedin.com" target="_blank" className="button secondary">
-                <img src="/images/icon__linkedin.svg" alt="logo do linkedin"/>
+              <a
+                href="https://www.linkedin.com/in/rafael-engelbrecht-ba271961/"
+                target="_blank"
+                className="button secondary"
+              >
+                <img src="/images/icon__linkedin.svg" alt="logo do linkedin" />
                 Veja mais no Linkedin
               </a>
             </div>
-            <div className="col areas__ilustracao">  
+            <div className="col areas__ilustracao">
               <img src="/images/ilustracao__areas.png" />
             </div>
           </div>
         </div>
       </div>
 
-    <div id="formacao">
-      <div className="container">
-        <h1>Formação Acadêmica</h1>
-        <h2>CURSOS DE GRADUAÇÃO, PÓS GRADUAÇÃO, ESPECIALIZAÇÕES E RESIDÊNCIAS</h2>
-        <div className="row">
-          <div className="col w50">
-            <div className="formacao__logos">
-                <img src="/images/marca__epm.png" alt="marca da escola paulista de medicina - escudo mostrando uma cobra enrolana num tronco de árvore" className="logo-epm"/>
-                <img src="/images/marca__unifesp.png" alt="marca da unifesp - fachada estilizada do prédio" className="logo-unifesp"/>
-            </div>
-            <div className="row formacao__cursos">
+      <div id="formacao">
+        <div className="container">
+          <h1>Formação Acadêmica</h1>
+          <h2>
+            CURSOS DE GRADUAÇÃO, PÓS GRADUAÇÃO, ESPECIALIZAÇÕES E RESIDÊNCIAS
+          </h2>
+          <div className="row">
+            <div className="col w50">
+              <div className="formacao__logos">
+                <img
+                  src="/images/marca__epm.png"
+                  alt="marca da escola paulista de medicina - escudo mostrando uma cobra enrolana num tronco de árvore"
+                  className="logo-epm"
+                />
+                <img
+                  src="/images/marca__unifesp.png"
+                  alt="marca da unifesp - fachada estilizada do prédio"
+                  className="logo-unifesp"
+                />
+              </div>
+              <div className="row formacao__cursos">
                 <ul>
                   <li>Graduação em Medicina</li>
                   <li>Residência em Cirurgia Geral</li>
                   <li>Residência em Cirurgia Vascular</li>
                   <li>Residência em Cirurgia Endovascular</li>
                 </ul>
-                
+              </div>
+            </div>
+            <div className="col w50 formacao__detalhes">
+              <p>
+                Ganhador do <strong>2º Prêmio Emil Burihan</strong> da Sociedade
+                Brasileira de Angiologia e Cirurgia Vascular no ano de 2016 (3º
+                lugar).
+              </p>
+              <p>
+                Possui experiência de trabalho no Hospital do Rim, com pacientes
+                transplantados renais em vias de acesso para hemodiálise.{" "}
+              </p>
+              <p>
+                Atualmente, trabalha como Cirurgião Vascular e Endovascular no
+                Hospital 9 de Julho com pacientes internados, retaguarda de
+                Pronto Socorro, cirurgias eletivas e consultas ambulatoriais.
+              </p>
+              <a
+                href="http://lattes.cnpq.br/3009961818693674"
+                target="_blank"
+                className="button secondary"
+              >
+                <img
+                  src="/images/icon__lattes.svg"
+                  alt="marca da plataforma lattes"
+                />
+                Currículo Lattes
+              </a>
             </div>
           </div>
-          <div className="col w50 formacao__detalhes">
-            <p>Ganhador do  <strong>2º Prêmio Emil Burihan</strong> da Sociedade Brasileira de Angiologia e Cirurgia Vascular no ano de 2016 (3º lugar).</p>
-            <p>Possui experiência de trabalho no Hospital do Rim, com pacientes transplantados renais em vias de acesso para hemodiálise. </p>
-            <p>Atualmente, trabalha como Cirurgião Vascular e Endovascular no Hospital 9 de Julho com pacientes internados, retaguarda de Pronto Socorro, cirurgias eletivas e consultas ambulatoriais.</p>
-            <a href="http://lattes.cnpq.br/" target="_blank" className="button secondary">
-                  <img src="/images/icon__lattes.svg" alt="marca da plataforma lattes" />
-                  Currículo Lattes
+        </div>
+      </div>
+
+      <div id="publicacao">
+        <div className="container">
+          <div className="row">
+            <div className="col w50">
+              <img
+                src="/images/livro.png"
+                alt="capa do livro"
+                className="publicacao__livro"
+              />
+            </div>
+            <div className="col w50 publicacao__text">
+              <h1>Publicação</h1>
+              <p>
+                <strong>
+                  Manual de Angiologia e Cirurgia Vascular e Endovascular (2020)
+                </strong>{" "}
+                - Editora Manole
+              </p>
+              <h3>Participação: </h3>
+              <ul>
+                <li>
+                  Capítulo 6: Propedêutica linfática ao lado de Luis Carlos Uta
+                  Nakano
+                </li>
+                <li>
+                  Capítulo 74: Complicações das angioplastias de carótida ao
+                  lado de Luis Carlos Uta Nakano
+                </li>
+                <li>
+                  Capítulo 84: Angioplastia da veia cava inferior e do segmento
+                  ileofemoral na insuficiência venosa crônicaao lado de Samuel
+                  Tomaz Araújo e Ronald Luiz Gomes Flumignan.
+                </li>
+              </ul>
+              <div className="buttons">
+                <a href="https://www.manole.com.br/manual-de-angiologia-e-cirurgia-vascular-e-endovascular/p" target="_blank" className="button secondary">
+                  <img src="/images/icon__book.svg" alt="icone de um livro" />
+                  Editora Manole
                 </a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div id="publicacao">
-      <div className="container">
-        <h1>Publicação</h1>
-        <div className="row">
-          <div className="col w50">
-            <img src='/images/livro.png' alt="capa do livro" className="publicacao__livro"/>
-            <div className="buttons">
-            <a href="#" className='button secondary'>
-              <img src="/images/icon__book.svg" alt="icone de um livro" />Amazon</a>
-            <a href="#" className='button secondary'>
-              <img src="/images/icon__book.svg" alt="icone de um livro" />Editora Manole</a>
+                <a href="https://www.amazon.com.br/MANUAL-ANGIOLOGIA-CIRURGIA-VASCULAR-ENDOVASCULAR/dp/8520460100" target="_blank" className="button secondary">
+                  <img src="/images/icon__book.svg" alt="icone de um livro" />
+                  Amazon
+                </a>
               </div>
-          </div>
-          <div className="col w50 publicacao__text">
-            <p><strong>Manual de Angiologia e Cirurgia Vascular e Endovascular (2020)</strong> - Editora Manole</p>
-            <h3>Participação: </h3>
-            <ul>
-              <li>Capítulo 6: Propedêutica linfática ao lado de Luis Carlos Uta Nakano</li>
-              <li>Capítulo 74: Complicações das angioplastias de carótida ao lado de Luis Carlos Uta Nakano</li>
-              <li>Capítulo 84: Angioplastia da veia cava inferior e do segmento ileofemoral na insuficiência venosa crônicaao lado de Samuel Tomaz Araújo e Ronald Luiz Gomes Flumignan.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div id="atendimento">
-    <div className="container">
-        <h1>Locais de Atendimento</h1>
-        <div className="row">
-          <div className="col w25">
-           <div className="atendimento__logo"> 
-            <img src="/images/logo__haoc.svg" alt="marca do hospital alemão oswaldo cruz"/></div>
-            <h3>Hospital Alemão Oswaldo Cruz</h3>
-          </div>
-          <div className="col w25">
-            <div className="atendimento__logo"><img src="/images/logo__hospital-sirio-libanes.svg"  alt="marca do hospital sírio libanês"/></div>
-            <h3>Hospital Sírio Libanês</h3>
-          </div>
-          <div className="col w25">
-            <div className="atendimento__logo"><img src="/images/logo__hospital-9-de-julho.svg"  alt="marca do hospital 9 de julho"/></div>
-            <h3>Hospital 9 de Julho</h3>
-          </div>
-          <div className="col w25">
-            <div className="atendimento__logo"><img src="/images/logo__consultorio.svg" alt="marca do consultorio particular"/></div>
-            <h3>Consultório Particular</h3>
+            </div>
           </div>
         </div>
       </div>
 
-      <div id="call_to_action">
+      <div id="atendimento">
+        <div className="container">
+          <h1>Locais de Atendimento</h1>
+          <div className="row">
+            <div className="col w25">
+              <div className="atendimento__logo">
+                <a
+                  href="https://www.hospitaloswaldocruz.org.br/"
+                  target="_blank"
+                >
+                  <img
+                    src="/images/logo__haoc.svg"
+                    alt="marca do hospital alemão oswaldo cruz"
+                  />
+                </a>
+              </div>
+              <h3>Hospital Alemão Oswaldo Cruz</h3>
+            </div>
+            <div className="col w25">
+              <div className="atendimento__logo">
+                <a href="https://www.hospitalsiriolibanes.org.br/Paginas/nova-home.aspx" target="_blank">
+                  <img
+                    src="/images/logo__hospital-sirio-libanes.svg"
+                    alt="marca do hospital sírio libanês"
+                  />
+                </a>
+              </div>
+              <h3>Hospital Sírio Libanês</h3>
+            </div>
+            <div className="col w25">
+              <div className="atendimento__logo">
+                <a href="https://www.h9j.com.br/Paginas/default.aspx" target="_blank">
+                  <img
+                    src="/images/logo__hospital-9-de-julho.svg"
+                    alt="marca do hospital 9 de julho"
+                  />
+                </a>
+              </div>
+              <h3>Hospital 9 de Julho</h3>
+            </div>
+            <div className="col w25">
+              <div className="atendimento__logo">
+                <a href="#call_to_action" tarket="_blank">
+                  <img
+                    src="/images/logo__consultorio.svg"
+                    alt="marca do consultorio particular"
+                  />
+                </a>
+              </div>
+              <h3>Consultório Particular</h3>
+            </div>
+          </div>
+        </div>
+
+        <div id="call_to_action">
           <div className="cta__icon">
             <img src="/images/cta__contact.png" />
           </div>
           <div className="cta__message">
-            Por favor, sinta-se a vontade para ligar em nossa clínica e conversar com o pessoal da recepção. Seu atendimento será rápido e atencioso.
+            Por favor, sinta-se a vontade para ligar em nossa clínica e
+            conversar com o pessoal da recepção. Seu atendimento será rápido e
+            atencioso.
             <a href="#">11 3995-9852</a>
           </div>
           <div className="cta__address">
-              <img src="/images/icon__place.svg" alt="alfinete de mapa" />
-              <p>
-              Rua Pamplona, 145 cj 134
-              Jardim Paulista
-              São Paulo SP
-              </p>
+            <img src="/images/icon__place.svg" alt="alfinete de mapa" />
+            <p>Rua Pamplona, 145 cj 134 Jardim Paulista São Paulo SP</p>
           </div>
           <div className="cta__button">
-            <a href="#" className="button primary">
-              <img src="/images/icon__arrow-right-squared.svg" alt="seta apontando para esquerda" />
+            <a href="tel:+5511952766726" className="button primary">
+              <img
+                src="/images/icon__arrow-right-squared.svg"
+                alt="seta apontando para esquerda"
+              />
               Marque sua consulta
             </a>
           </div>
+        </div>
       </div>
-    </div>
-   
-    <div id="footer">
-      <div className="container">
-        <img src="/images/brand__white.png" alt="marca do consultaorio" />
-        <p>Esse site e seu conteúdo estão protegidos por leis de deireito autoral, tanto no brasil quanto no exterior. Antes de reproduzí-lo, no todo ou em parte, por favor entre em contato.</p>
-        <a href="#top">Voltar ao inicio</a>
-      </div>
-    </div>
-  </div>
 
-  )
+      <div id="footer">
+        <div className="container">
+          <img src="/images/brand__white.png" alt="marca do consultaorio" />
+          <p>
+            Esse site e seu conteúdo estão protegidos por leis de deireito
+            autoral, tanto no brasil quanto no exterior. Antes de reproduzí-lo,
+            no todo ou em parte, por favor entre em contato.
+          </p>
+          <a href="#top">Voltar ao inicio</a>
+        </div>
+      </div>
+    </div>
+  );
 }
